8c193f3bd64271eb6af079ffafc438b0766f4c72
Konstantinos Nikoloutsos <konstantinos.nikoloutsos@vodafone.com>
2021-11-13 22:23:44 +0200

    Add ability for location animation

--- FancyGradient/FancyGradient/FancyGradientView+Animation.swift
+++ FancyGradient/FancyGradient/FancyGradientView+Animation.swift
@@ -80,6 +80,26 @@ public class DirectionAnimation: Animationable {
     }
 }
 
+/// Animation for colorStops of gradient
+public class ColorStopsAnimation: Animationable {
+    let colorStops: [Double]?
+    public let duration: Double
+
+    lazy public var caAnimation: CAAnimation = {
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.toValue = colorStops?.map{ NSNumber(value: $0) }
+        animation.duration = duration
+        animation.fillMode = .forwards;
+        animation.repeatCount = 1
+        return animation
+    }()
+
+    public init(colorStops: [Double]?, duration: Double) {
+        self.colorStops = colorStops
+        self.duration = duration
+    }
+}
+
 /// Animation for animating the color of the gradient
 public class ColorAnimation: Animationable {
     let colors: [UIColor]
--- FancyGradient/FancyGradient/FancyGradientView.swift
+++ FancyGradient/FancyGradient/FancyGradientView.swift
@@ -11,7 +11,7 @@ import UIKit
 // MARK: - FancyGradientView
 /// An animated GradientView. Supports custom animations and will make your UI look fancy and sexy 🚀
 public final class FancyGradientView: UIView {
-    /// When provided animID to your CustomAnimation this delegate will notify you about animation finish.
+    /// When provided animID to your CustomAnimation this delegate will notify you about animation completion.
     public weak var delegate: FancyGradientViewDelegate?
 
     /// The colors that the gradient is going to have.  🎨
@@ -35,6 +35,14 @@ public final class FancyGradientView: UIView {
         }
     }
 
+    /// The gradient stops are specified as values between 0 and 1.
+    /// The values must be monotonically increasing. If nil, the stops are spread uniformly across the range.
+    public var colorStops: [Double]? {
+        didSet {
+            applyGradient()
+        }
+    }
+
     var gradientLayer: CAGradientLayer {
          self.layer as! CAGradientLayer
     }
@@ -47,12 +55,16 @@ public final class FancyGradientView: UIView {
     /// - Parameters:
     ///   - colors: Gradient colors  (e.g [black, red])
     ///   - direction: Direction of gradient  (e.g .up)
+    ///   - colorStops: Determines the space that each color is going to occupy on gradient.
+    ///   Values must be between 0 and 1. Number of values must be equal to number of `colors`.
     ///   - type: Gradient type (e.g axial)
     public init(colors: [UIColor],
                 direction: Direction,
-                type: CAGradientLayerType) {
+                colorStops: [Double]? = nil,
+                type: CAGradientLayerType = .axial) {
         self.colors = colors
         self.direction = direction
+        self.colorStops = colorStops
         self.type = type
         super.init(frame: .zero)
         applyGradient()
@@ -73,6 +85,7 @@ public final class FancyGradientView: UIView {
         gradientLayer.startPoint = direction.cgPoints.0
         gradientLayer.endPoint = direction.cgPoints.1
         gradientLayer.type = type
+        gradientLayer.locations = colorStops?.map{ NSNumber(value: $0) }
     }
 }
 
