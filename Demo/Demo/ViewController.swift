b93ea6dbf280ef40cb2d40c173a7fb875a89310a
Konstantinos Nikoloutsos <123123clapyourhands@gmail.com>
2021-11-16 23:54:00 +0200

    feat: update demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -14,53 +14,89 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // MARK: Add the fancyView in screen*/
-        fancyView = FancyGradientView(colors: [UIColor.black, UIColor.orange], direction: .diagonalTopLeftBottomRight, type: .axial)
-            .newAutoLayout()
+        fancyView = FancyGradientView(colors: [UIColor.blue, UIColor.green],
+                                      direction: .down,
+                                      colorStops: [0, 1],
+                                      type: .axial).newAutoLayout()
         fancyView.delegate = self
 
         view.addSubview(fancyView)
         fancyView.pinEdges(to: view)
 
-
         // Enable one at a time!
-//        playExampleAnimation1()
-//        playExampleAnimation2()
-//        playExampleAnimation3()
-//        playExampleAnimation4()
-        playExampleAnimation5()
+
+        demo()
+//        demo2()
+//        demo3()
+//        demo4()
+//        demo5()
     }
 
 
-    func playExampleAnimation1() {
+
+    func demo() {
         let myCustomAnimation = CustomAnimation(animID: "myAnimationID")
-            .then(EmptyAnimation(duration: 1)) // Delay 1 second
-            .then(DirectionAnimation(newDirection: .right, duration: 2))
+            .then(EmptyAnimation(duration: 0.5))
+            .then(ColorStopsAnimation(colorStops: [1, 1], duration: 1))
+            .then(DirectionAnimation(newDirection: .down, duration: 0.3))
+            .then(CombineAnimation(
+                ColorAnimation(newColors: [.init(hexString: "#fa709a"), .init(hexString: "#fee140")], duration: 1),
+                ColorStopsAnimation(colorStops: [0.1, 0.9], duration: 1)
+            ))
+            .then(ColorStopsAnimation(colorStops: [1, 1], duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalTopLeftBottomRight, duration: 0.3))
+            .then(CombineAnimation(
+                ColorAnimation(newColors: [.init(hexString: "#00bf8f"), .init(hexString: "#001510")], duration: 3),
+                ColorStopsAnimation(colorStops: [0.2, 0.8], duration: 3)
+            ))
+            .then(DirectionAnimation(newDirection: .down, duration: 1))
+            .then(ColorStopsAnimation(colorStops: [0.1, 0.9], duration: 1))
+
         fancyView.animate(animation: myCustomAnimation)
     }
 
-    func playExampleAnimation2() { // This is like the direction does a full cycle
+    func demo2() {
         let myCustomAnimation = CustomAnimation(animID: "myAnimationID")
-            .then(EmptyAnimation(duration: 1)) // Delay 1 second
-            .then(DirectionAnimation(newDirection: .right, duration: 1))
-            .then(DirectionAnimation(newDirection: .diagonalBottomLeftTopRight, duration: 1))
-            .then(DirectionAnimation(newDirection: .up, duration: 1))
-            .then(DirectionAnimation(newDirection: .diagonalBottomRightTopLeft, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalTopRightBottomLeft, duration: 1))
             .then(DirectionAnimation(newDirection: .left, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalBottomRightTopLeft, duration: 1))
+            .then(DirectionAnimation(newDirection: .up, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalBottomLeftTopRight, duration: 1))
+            .then(DirectionAnimation(newDirection: .right, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalTopLeftBottomRight, duration: 1))
+            .then(DirectionAnimation(newDirection: .down, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalTopRightBottomLeft, duration: 1))
+            .then(DirectionAnimation(newDirection: .left, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalBottomRightTopLeft, duration: 1))
+            .then(DirectionAnimation(newDirection: .up, duration: 0.5))
+            .then(DirectionAnimation(newDirection: .diagonalBottomLeftTopRight, duration: 1))
+            .then(DirectionAnimation(newDirection: .right, duration: 1))
+            .then(DirectionAnimation(newDirection: .diagonalTopLeftBottomRight, duration: 1))
+            .then(DirectionAnimation(newDirection: .down, duration: 1))
+
         fancyView.animate(animation: myCustomAnimation)
     }
 
 
-    func playExampleAnimation3() {
+    func demo3() {
         let myCustomAnimation = CustomAnimation(animID: "myAnimationID")
+            .then(EmptyAnimation(duration: 1)) // delay 1 second
+            .then(DirectionAnimation(newDirection: .up, duration: 3))
+        fancyView.animate(animation: myCustomAnimation)
+    }
+
+    func demo4() {
+        let myCustomAnimation = CustomAnimation()
             .then(EmptyAnimation(duration: 1)) // Delay 1 second
             .then(ColorAnimation(newColors: [UIColor.blue, UIColor.cyan], duration: 3))
             .then(ColorAnimation(newColors: [UIColor.cyan, UIColor.blue], duration: 3))
             .then(ColorAnimation(newColors: [UIColor.purple, UIColor.blue, UIColor.black], duration: 3))
+            .then(DirectionAnimation(newDirection: .up, duration: 2))
         fancyView.animate(animation: myCustomAnimation)
     }
 
 
-    func playExampleAnimation4() {
+    func demo5() {
         let myCustomAnimation = CustomAnimation(animID: "myAnimationID")
             .then(EmptyAnimation(duration: 1)) // Delay 1 second
             .then(
@@ -71,17 +107,6 @@ class ViewController: UIViewController {
             )
             .then(DirectionAnimation(newDirection: .right, duration: 2))
             .then(DirectionAnimation(newDirection: .diagonalBottomLeftTopRight, duration: 2))
-        fancyView.animate(animation: myCustomAnimation)
-    }
-
-    func playExampleAnimation5() {
-        let myCustomAnimation = CustomAnimation(animID: "myAnimationID")
-            .then(EmptyAnimation(duration: 1)) // Delay 1 second
-            .then(ColorAnimation(newColors: [UIColor.black, UIColor.green], duration: 1))
-            .then(ColorAnimation(newColors: [UIColor.black, UIColor.black, UIColor.green], duration: 1))
-            .then(ColorAnimation(newColors: [UIColor.black, UIColor.green], duration: 1))
-            .then(ColorAnimation(newColors: [UIColor.black, UIColor.black, UIColor.green], duration: 1))
-            .then(ColorAnimation(newColors: [UIColor.black, UIColor.green], duration: 1))
         fancyView.animate(animation: myCustomAnimation)
     }
 }
