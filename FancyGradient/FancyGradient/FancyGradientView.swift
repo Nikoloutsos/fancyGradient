8ae3382f3a649a9f7bb59bece35bca6778681ef4
Konstantinos Nikoloutsos <123123clapyourhands@gmail.com>
2021-11-17 22:06:28 +0200

    feat: Allow creation form xibs (#14)

--- FancyGradient/FancyGradient/FancyGradientView.swift
+++ FancyGradient/FancyGradient/FancyGradientView.swift
@@ -71,7 +71,7 @@ public final class FancyGradientView: UIView {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 
     public override func awakeFromNib() {
